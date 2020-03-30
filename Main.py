6bb3c9b0157f636fe83d9b45ff41f0412d94e018
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-03-30 19:36:27 -0300

    Criada Classe BotBBB

--- Main.py
+++ Main.py
@@ -1,11 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-options = Options()
-options.headless = True
-browser = webdriver.Firefox(options=options)
-browser.get('https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-felipe-manu-ou-mari-a9f49f90-84e2-4c12-a9af-b262e2dd5be4.ghtml')
+class BotBBB:
+    def __init__(self,URL:str):
+        self.URL = URL
+        options = Options()
+        options.headless = True
+        self.browser = webdriver.Firefox(options=options)
+        self.browser.get(URL)    
 
-print(browser.title)
+    def getTitle(self):
+        return self.browser.title
 
-browser.quit()
+    def getParticipantes(self):
+        repeat = 20*'='
+        print('{} ENCONTRANDO PARTICIPANTES {}'.format(repeat, repeat))
+        elements = self.browser.find_elements_by_class_name('_3l2BjojXxJ4MNadPvFe7Tk')
+        print(self.browser.title)
+        print(len(elements))
+        for elem in elements:
+            print(type(elem))
+    
+    def quit(self):
+        self.browser.quit()
+
+
+if __name__ == "__main__":
+    URL = 'https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-felipe-manu-ou-mari-a9f49f90-84e2-4c12-a9af-b262e2dd5be4.ghtml'
+    bot = BotBBB(URL)
+    bot.getParticipantes()
+    print(bot.getTitle())
+    
+    bot.quit()
